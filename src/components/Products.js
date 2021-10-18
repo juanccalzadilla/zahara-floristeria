49fe4b4e61732c1a52bf856815712cc270b1792f
Juan Calzadilla <juancalza91@gmail.com>
2021-10-18 20:33:40 +0200

    fix category bug

--- src/components/Products.js
+++ src/components/Products.js
@@ -6,21 +6,21 @@ export const Products = () => {
         <div className="container">
             <h1>Aqui podrás encontrar...</h1>
             <div className="row mx-auto w-100">
-                <Link to='/categoria/rosas' className="col-md-auto mx-auto btn">
+                <Link to='/categoria/rosas' className="col-md-4 mx-auto btn">
                     <img src="https://tinyurl.com/roses00001" className="img-fluid" width="400" height="400" alt=""></img>
                     <h2 className="text-center">Ramo de Rosas</h2>
 
                 </Link>
-                <Link to='/categoria/flores' className="col-md-auto btn">
+                <Link to='/categoria/flores' className="col-md-4 btn">
                     <img src="https://tinyurl.com/bouqets102" className="img-fluid" width="400" height="400" alt=""></img>
                     <h2 className="text-center">Ramo de flores</h2>
 
 
                 </Link>
-                <div className="col-md-auto">
+                <Link to='/categoria/plantas'className="col-md-4 btn">
                     <img src="https://tinyurl.com/0004flor" className="img-fluid" width="400" height="400" alt=""></img>
                     <h2 className="text-center">Plantas</h2>
-                </div>
+                </Link>
             </div>
 
             <Link to="/productos" className="btn btn-outline-danger w-100 mt-3 mb-3">Ver Mas</Link>
