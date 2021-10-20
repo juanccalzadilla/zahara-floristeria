61fc1f5c9b3b5dc37a838e65c4e7c62d48b8faf5
Juan Calzadilla <juancalza91@gmail.com>
2021-10-20 20:29:15 +0200

    Cards design updated

--- src/components/Categoria.js
+++ src/components/Categoria.js
@@ -1,4 +1,5 @@
 import hero2 from '../assets/hero2.jpg'
+import './Categoria.css'
 import React from 'react'
 import { useParams } from 'react-router'
 const rosasData = require('../json/rosas')
@@ -10,25 +11,34 @@ export const Categoria = () => {
     console.log(data);
     return (
         <>
-        <h1 className="text-center bg text p-4">Perfect Store - Products</h1>
-        <div className="container-fluid d-flex flex-wrap">
-            <div className="row row-cols-1 row-cols-md-3 row-cols-lg-5
+            <h1 className="text-center bg-warning text p-4">Floristeria Zahara - {category.toUpperCase()}</h1>
+            <div className="container-fluid d-flex flex-wrap">
+                <div className="row row-cols-1 row-cols-md-3 row-cols-lg-5
             justify-content-center">
-                {data.map(data => (
-                    
-                    <div className="card-container d-flex txt flex-column m-4" key={data.id} >
-                        <img src={hero2} alt={data.name}  className="img-fluid" />
+                    {data.map(data => (
+
+                        <div className="card-container d-flex txt flex-column m-4" key={data.id} >
+
+                           <div className="my-card">
+                               <img className="img-fluid" src={hero2}></img>
+                               <h1 className="name">{data.name}</h1>
+                               <p className="price"> €{data.price}</p>
+                           </div>
+
+                            {/* <img src={hero2} alt={data.name}  className="img-fluid image" />
                         <div className="info">
                             <h3 className="text-center mt-3">{data.name}</h3>
                             <p>{data.description}</p>
                             <span className="text-center">---------------------&gt;€{data.price}</span>
-                            <button className="btn btn-danger w-100 mr-3 ">BUY NOW</button>
+                            
+                        </div> */}
                         </div>
-                    </div>
 
-                ))}
+
+                    ))}
+                    <button className="btn btn-danger w-100 mr-3 ">Contacto</button>
+                </div>
             </div>
-        </div>
-    </>
+        </>
     )
 }
