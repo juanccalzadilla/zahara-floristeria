37bd58892f17d23127001955bc6267cce134bf3e
Juan Calzadilla <juancalza91@gmail.com>
2021-10-21 19:45:30 +0200

    Update

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ import './css/header.css'
 export const Header = () => {
     return (
         <div className="bg-danger">
-            <h1 className="text-white">Floristeria Zaharaa</h1>
+            <h1 className="text-white">Floristeria Zahara</h1>
         </div>
     )
 }
