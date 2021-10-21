8aada4c5457e052440a1e5e1853a2402fffa44ae
Juan Calzadilla <juancalza91@gmail.com>
2021-10-21 19:25:21 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -5,17 +5,20 @@ import {
     Route,Redirect
 } from "react-router-dom";
 import { Footer } from './components/helperComponents/Footer'
-import { Header } from './components/Header'
+
 import { NavBar } from './components/helperComponents/NavBar'
 import { HomeScreen } from './components/Screens/HomeScreen'
 import { Productos } from './components/Screens/Productos'
 import { Galeria } from './components/Screens/Galeria'
 import { Contacto } from './components/Screens/Contacto'
 import { Categoria } from './components/Categoria';
+import { Header } from './components/Header';
+
+
 export const App = () => {
     return (
         <Router>
-            <Header />
+            <Header/>
             <NavBar />
             <Switch>
                 <Route exact path="/" component={HomeScreen}/>
--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './css/header.css'
+
 export const Header = () => {
     return (
         <div className="bg-danger">
